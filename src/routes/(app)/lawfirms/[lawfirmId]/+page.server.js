4f7b729904c5c0a57481d12c93d2afb334d7fb92
David <dave28221.@hotmail.com>
2025-03-06 12:12:55 +0800

    test env

--- src/routes/(app)/lawfirms/[lawfirmId]/+page.server.js
+++ src/routes/(app)/lawfirms/[lawfirmId]/+page.server.js
@@ -1,42 +1,42 @@
-// import mailchimp from '@mailchimp/mailchimp_marketing';
+import mailchimp from '@mailchimp/mailchimp_marketing';
 
 const apiKey = process.env.VITE_MAILCHIMP_API_KEY || 'default';
 
-// if (!apiKey) {
-//     console.error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
-//     throw new Error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
-// }
+if (!apiKey) {
+    console.error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
+    throw new Error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
+}
 
-// const apiKeyParts = apiKey.split('-');
+const apiKeyParts = apiKey.split('-');
 
-// if (apiKeyParts.length < 2) {
-//     console.error('VITE_MAILCHIMP_API_KEY is not in the correct format');
-//     throw new Error('VITE_MAILCHIMP_API_KEY is not in the correct format');
-// }
+if (apiKeyParts.length < 2) {
+    console.error('VITE_MAILCHIMP_API_KEY is not in the correct format');
+    throw new Error('VITE_MAILCHIMP_API_KEY is not in the correct format');
+}
 
-// const serverPrefix = apiKeyParts[1]; // Extract the data center
+const serverPrefix = apiKeyParts[1]; // Extract the data center
 
-// mailchimp.setConfig({
-//     apiKey: apiKey,
-//     server: serverPrefix,
-// });
+mailchimp.setConfig({
+    apiKey: apiKey,
+    server: serverPrefix,
+});
 
-// export async function load() {
-//     try {
-//         const response = await mailchimp.campaigns.list({ count: 1 });
-//         const responseTwo = await mailchimp.reports.getAllCampaignReports();
-//         return {
-//             response,
-//             responseTwo
-//         };
-//     } catch (error) {
-//         console.error('Error fetching Mailchimp data:', error);
-//         return {
-//             status: 500,
-//             body: {
-//                 error: 'Failed to fetch Mailchimp data',
-//                 details: error.message
-//             }
-//         };
-//     }
-// }
+export async function load() {
+    try {
+        const response = await mailchimp.campaigns.list({ count: 1 });
+        const responseTwo = await mailchimp.reports.getAllCampaignReports();
+        return {
+            response,
+            responseTwo
+        };
+    } catch (error) {
+        console.error('Error fetching Mailchimp data:', error);
+        return {
+            status: 500,
+            body: {
+                error: 'Failed to fetch Mailchimp data',
+                details: error.message
+            }
+        };
+    }
+}
