2517b7792fe682da219f31a8ced9e10d0d4d8fd2
David <dave28221.@hotmail.com>
2025-03-06 12:32:21 +0800

    sdfs

---
+++ src/routes/(app)/lawfirms/[lawfirmId]/+page.server.js
@@ -0,0 +1,42 @@
+import mailchimp from '@mailchimp/mailchimp_marketing';
+
+const apiKey = process.env.VITE_MAILCHIMP_API_KEY;
+
+if (!apiKey) {
+    console.error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
+    throw new Error('VITE_MAILCHIMP_API_KEY is not defined in .env file');
+}
+
+const apiKeyParts = apiKey.split('-');
+
+if (apiKeyParts.length < 2) {
+    console.error('VITE_MAILCHIMP_API_KEY is not in the correct format');
+    throw new Error('VITE_MAILCHIMP_API_KEY is not in the correct format');
+}
+
+const serverPrefix = apiKeyParts[1]; // Extract the data center
+
+mailchimp.setConfig({
+    apiKey: apiKey,
+    server: serverPrefix,
+});
+
+export async function load() {
+    try {
+        const response = await mailchimp.campaigns.list({ count: 1 });
+        const responseTwo = await mailchimp.reports.getAllCampaignReports();
+        return {
+            response,
+            responseTwo
+        };
+    } catch (error) {
+        console.error('Error fetching Mailchimp data:', error);
+        return {
+            status: 500,
+            body: {
+                error: 'Failed to fetch Mailchimp data',
+                details: error.message
+            }
+        };
+    }
+}
