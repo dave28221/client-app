bc719dde46d0a8b7b0b87ee5537325b76256703e
David <dave28221.@hotmail.com>
2025-03-06 12:37:26 +0800

    push

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-		  external: ['papaparse'],
-		},
-	  },
+
+	
 });
+// Compare this snippet from src/routes/%28app%29/upload/%2Bpage.svelte:
+// <script context="module">
+// 	export { load } from './upload/+page.server.js';			
